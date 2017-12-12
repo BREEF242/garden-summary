f765c657fed51a5b09aba7d071c3fdb301933c60
Alex Coppen <acamerondev@protonmail.com>
2017-12-12 18:52:50 -0500

    Spelling

--- resources/views/sections/front.blade.php
+++ resources/views/sections/front.blade.php
@@ -5,7 +5,7 @@
         <div class="text-center">
           <h1 class="title large object-non-visible" data-animation-effect="zoomIn" data-effect-delay="300">Sir Nicholas Nuttall Coral Reef Sculpture Garden</h1>
           <div class="separator with-icon"><i class="fa vs vs-fish bordered" aria-hidden="true"></i></div>
-          <p class="object-non-visible" data-animation-effect="fadeInDown" data-effect-delay="200">The Bahamas Reef Environment Education Foundation (BREEF) created the <a target="_blank" href="https://breef.org/public-awareness/the-coral-reef-sculpture-garden/">country’s first underwater living art gallery</a> off the south west coast of New Providence near the <a target="_blank" href="https://www.bahamas.com/vendor/clifton-heritage-park">Clifton Heritage National Park</a>.
+          <p class="object-non-visible" data-animation-effect="fadeInDown" data-effect-delay="200">The Bahamas Reef Environment Educational Foundation (BREEF) created the <a target="_blank" href="https://breef.org/public-awareness/the-coral-reef-sculpture-garden/">country’s first underwater living art gallery</a> off the south west coast of New Providence near the <a target="_blank" href="https://www.bahamas.com/vendor/clifton-heritage-park">Clifton Heritage National Park</a>.
           </p>
           <a href="#why" class="btn btn-lg moving smooth-scroll"><i class="icon-down-open-big"></i><i class="icon-down-open-big"></i></a>
         </div>
